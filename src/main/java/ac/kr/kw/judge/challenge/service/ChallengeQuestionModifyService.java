65b0e02fca76f54a2ed9782817d4b7f2ac20e5cf
CodeCommit-at-308987748304 <annyousung@gmail.com>
2022-02-07 17:56:25 +0900

    feat

--- src/main/java/ac/kr/kw/judge/challenge/service/ChallengeQuestionModifyService.java
+++ src/main/java/ac/kr/kw/judge/challenge/service/ChallengeQuestionModifyService.java
@@ -1,5 +1,11 @@
 package ac.kr.kw.judge.challenge.service;
 
+import ac.kr.kw.judge.challenge.service.command.ChallengeInfoModifyCommand;
+
 public interface ChallengeQuestionModifyService {
-    Long registerChallenge();
+    Long addQuestion(Long challengeId, Long problemId, String title);
+
+    void deleteQuestion(Long challengeId, Long questionId);
+
+    void changeChallengeInfo(Long challengeId, ChallengeInfoModifyCommand challengeInfoModifyCommand);
 }
