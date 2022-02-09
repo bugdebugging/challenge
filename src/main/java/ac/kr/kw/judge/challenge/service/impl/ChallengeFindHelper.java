ffae901fffd7169219bb6fad7e408f0a97dda324
CodeCommit-at-308987748304 <annyousung@gmail.com>
2022-02-09 12:18:18 +0900

    feat:코드 제출 및 채점 완료구현

--- src/main/java/ac/kr/kw/judge/challenge/service/impl/ChallengeFindHelper.java
+++ src/main/java/ac/kr/kw/judge/challenge/service/impl/ChallengeFindHelper.java
@@ -3,8 +3,11 @@ package ac.kr.kw.judge.challenge.service.impl;
 import ac.kr.kw.judge.challenge.domain.Challenge;
 import ac.kr.kw.judge.challenge.repository.ChallengeRepository;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 public class ChallengeFindHelper {
     public static Challenge findChallengeById(Long challengeId, ChallengeRepository challengeRepository) {
+        checkArgument(challengeId != null, "대회 id는 필수입니다.");
         return challengeRepository.findById(challengeId)
                 .orElseThrow(() -> {
                     throw new IllegalArgumentException(challengeId + "의 대회가 존재하지 않습니다.");
---
+++ src/main/java/ac/kr/kw/judge/challenge/service/impl/SubmitSolutionServiceImpl.java
@@ -0,0 +1,39 @@
+package ac.kr.kw.judge.challenge.service.impl;
+
+import ac.kr.kw.judge.challenge.domain.Challenge;
+import ac.kr.kw.judge.challenge.domain.Participation;
+import ac.kr.kw.judge.challenge.repository.ChallengeRepository;
+import ac.kr.kw.judge.challenge.repository.ParticipationRepository;
+import ac.kr.kw.judge.challenge.service.SubmitSolutionService;
+import ac.kr.kw.judge.challenge.service.command.CompleteGradingSubmitCommand;
+import ac.kr.kw.judge.challenge.service.command.SolutionSubmitCommand;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class SubmitSolutionServiceImpl implements SubmitSolutionService {
+    private final ChallengeRepository challengeRepository;
+    private final ParticipationRepository participationRepository;
+
+    @Override
+    public void submitSolution(Long challengeId, SolutionSubmitCommand solutionSubmitCommand) {
+        Challenge challenge = ChallengeFindHelper.findChallengeById(challengeId, challengeRepository);
+        challenge.submitSolutionOfQuestion(solutionSubmitCommand.getParticipationId()
+                , solutionSubmitCommand.getProblemId()
+                , solutionSubmitCommand.getProgrammingLanguage()
+                , solutionSubmitCommand.getSourceCode());
+    }
+
+    @Override
+    public void completeGradingOfSubmit(Long participationId, CompleteGradingSubmitCommand completeGradingSubmitCommand) {
+        Participation participation = ParticipationFindHelper.findParticipationById(participationId, participationRepository);
+        participation.completeGradingOfSubmit(completeGradingSubmitCommand.getSubmitId()
+                , completeGradingSubmitCommand.getStatus()
+                , completeGradingSubmitCommand.getChallengeScore());
+    }
+}
