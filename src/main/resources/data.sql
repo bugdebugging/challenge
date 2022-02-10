43b3cb3671d52e0c56d02dd105cb7550fe51403f
CodeCommit-at-308987748304 <annyousung@gmail.com>
2022-02-10 14:54:33 +0900

    chore:data,schema.sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into challenges (name, start_time, end_time, created_at, updated_at)
+values ('round511 Div2', '2022-02-05 12:00:00', '2022-02-05 15:00:00', now(), now());
+
+insert into challenges (name, start_time, end_time, created_at, updated_at)
+values ('round512 Div3', '2022-02-01 18:00:00', '2022-02-01 20:00:00', now(), now());
+
+insert into challenges (name, start_time, end_time, created_at, updated_at)
+values ('round513 Div1', '2022-01-18 16:00:00', '2022-01-18 18:00:00', now(), now());
+
+insert into challenges (name, start_time, end_time, created_at, updated_at)
+values ('Educational Round', '2022-03-01 12:00:00', '2022-03-01 14:00:00', now(), now());
+
+insert into challenges (name, start_time, end_time, created_at, updated_at)
+values ('Global Round', '2022-03-02 12:00:00', '2022-03-02 14:00:00', now(), now());
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,63 @@
+DROP TABLE IF EXISTS challenge_authors;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS submits;
+DROP TABLE IF EXISTS participations;
+DROP TABLE IF EXISTS challenge;
+
+create table challenges
+(
+    id         bigint       NOT NULL AUTO_INCREMENT,
+    end_time   timestamp    NOT NULL,
+    start_time timestamp    NOT NULL,
+    created_at timestamp    NOT NULL,
+    name       varchar(255) NOT NULL,
+    updated_at timestamp    NOT NULL,
+    primary key (id),
+    INDEX (start_time)
+);
+
+create table challenge_authors
+(
+    challenge_id     bigint not null,
+    accumulate_score integer,
+    name             varchar(255),
+    user_id          bigint,
+    FOREIGN KEY (challenge_id) REFERENCES challenges (id)
+);
+
+create table participations
+(
+    id           bigint       NOT NULL AUTO_INCREMENT,
+    score        integer      NOT NULL default 0,
+    name         varchar(255) NOT NULL,
+    user_id      bigint       NOT NULL,
+    challenge_id bigint       NOT NULL,
+    primary key (id),
+    foreign key (challenge_id) REFERENCES challenges (id)
+);
+
+
+create table questions
+(
+    id           bigint       NOT NULL AUTO_INCREMENT,
+    problem_id   bigint       NOT NULL,
+    title        varchar(255) NOT NULL,
+    challenge_id bigint       NOT NULL,
+    primary key (id),
+    foreign key (challenge_id) REFERENCES challenges (id)
+);
+
+
+create table submits
+(
+    id                   bigint       NOT NULL AUTO_INCREMENT,
+    problem_id           bigint       NOT NULL,
+    programming_language varchar(255) NOT NULL,
+    score                integer      NOT NULL default 0,
+    source_code          varchar(255) NOT NULL,
+    submit_status        integer      NOT NULL,
+    submitted_at         timestamp    NOT NULL,
+    participation_id     bigint       NOT NULL,
+    primary key (id),
+    foreign key (participation_id) REFERENCES participations (id)
+);
