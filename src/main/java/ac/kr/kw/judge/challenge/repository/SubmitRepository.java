f66bd154c7024309e9d2190eb6f8e881ddd6f4d9
CodeCommit-at-308987748304 <annyousung@gmail.com>
2022-02-11 15:47:14 +0900

    feat:참여자의 제출 목록 조회

--- src/main/java/ac/kr/kw/judge/challenge/repository/SubmitRepository.java
+++ src/main/java/ac/kr/kw/judge/challenge/repository/SubmitRepository.java
@@ -1,7 +1,11 @@
 package ac.kr.kw.judge.challenge.repository;
 
+import ac.kr.kw.judge.challenge.domain.Participation;
 import ac.kr.kw.judge.challenge.domain.Submit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SubmitRepository extends JpaRepository<Submit, Long> {
+    List<Submit> findSubmitByParticipation(Participation participation);
 }
