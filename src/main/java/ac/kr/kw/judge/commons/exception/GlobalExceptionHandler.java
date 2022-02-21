55c42ec5d41185560d6fc28040e45f5e21004de9
CodeCommit-at-308987748304 <annyousung@gmail.com>
2022-02-21 12:30:35 +0900

    feat:business exception handler

--- src/main/java/ac/kr/kw/judge/commons/exception/GlobalExceptionHandler.java
+++ src/main/java/ac/kr/kw/judge/commons/exception/GlobalExceptionHandler.java
@@ -1,12 +1,15 @@
 package ac.kr.kw.judge.commons.exception;
 
+import ac.kr.kw.judge.commons.api.ApiResult;
+import ac.kr.kw.judge.commons.api.ApiUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler({IllegalStateException.class,IllegalArgumentException.class})
-    public void v(){
-
+    @ExceptionHandler({IllegalStateException.class, IllegalArgumentException.class})
+    public ApiResult handleBusinessException(Exception e) {
+        return ApiUtils.fail(HttpStatus.BAD_REQUEST.value(), e.getMessage());
     }
 }
