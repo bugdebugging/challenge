6ceb34b167377e2cda9ed3c746a4f13f8f725f6d
CodeCommit-at-308987748304 <annyousung@gmail.com>
2022-02-21 12:32:30 +0900

    fix:ResponseEntity 추가

--- src/main/java/ac/kr/kw/judge/commons/exception/GlobalExceptionHandler.java
+++ src/main/java/ac/kr/kw/judge/commons/exception/GlobalExceptionHandler.java
@@ -3,13 +3,15 @@ package ac.kr.kw.judge.commons.exception;
 import ac.kr.kw.judge.commons.api.ApiResult;
 import ac.kr.kw.judge.commons.api.ApiUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler({IllegalStateException.class, IllegalArgumentException.class})
-    public ApiResult handleBusinessException(Exception e) {
-        return ApiUtils.fail(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    public ResponseEntity<ApiResult> handleBusinessException(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiUtils.fail(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
     }
 }
