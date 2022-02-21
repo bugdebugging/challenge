990a7ae4ea8014e9807bc5f19001a09d9002d9ef
CodeCommit-at-308987748304 <annyousung@gmail.com>
2022-02-21 14:27:16 +0900

    dependency package changed

--- src/test/java/ac/kr/kw/judge/repository/ChallengeRepositoryTest.java
+++ src/test/java/ac/kr/kw/judge/repository/ChallengeRepositoryTest.java
@@ -2,9 +2,7 @@ package ac.kr.kw.judge.repository;
 
 import ac.kr.kw.judge.challenge.domain.Author;
 import ac.kr.kw.judge.challenge.domain.Challenge;
-import ac.kr.kw.judge.challenge.domain.Question;
-import ac.kr.kw.judge.challenge.dto.ChallengeListItemDto;
-import ac.kr.kw.judge.challenge.dto.ChallengeQuestionDto;
+import ac.kr.kw.judge.challenge.dto.out.ChallengeListItemDto;
 import ac.kr.kw.judge.challenge.repository.ChallengeRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
