76e675d800d4ba943277f55c6ac30bb7f0db7ece
bugdebugging <annyousung@gmail.com>
2022-03-22 13:42:42 +0900

    fix: problemclientimpl 코드정리

--- src/main/java/ac/kr/kw/judge/challenge/adapter/out/web/ProblemClientImpl.java
+++ src/main/java/ac/kr/kw/judge/challenge/adapter/out/web/ProblemClientImpl.java
@@ -13,7 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.server.ServerErrorException;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.LinkedHashMap;
