24504af3c5320a1d2c0e448cd84ec5c1ea918a5e
bugdebugging <annyousung@gmail.com>
2022-03-25 17:32:02 +0900

    feat:대회 시간 수정시 문제 open 시간수정 이벤트 발행

--- src/main/java/ac/kr/kw/judge/challenge/service/ChallengeModifyServiceImpl.java
+++ src/main/java/ac/kr/kw/judge/challenge/service/ChallengeModifyServiceImpl.java
@@ -3,10 +3,12 @@ package ac.kr.kw.judge.challenge.service;
 import ac.kr.kw.judge.challenge.domain.Author;
 import ac.kr.kw.judge.challenge.domain.Challenge;
 import ac.kr.kw.judge.challenge.domain.Question;
+import ac.kr.kw.judge.challenge.domain.event.ProblemOpenTimeChanged;
 import ac.kr.kw.judge.challenge.repository.ChallengeRepository;
 import ac.kr.kw.judge.challenge.service.port.in.ChallengeModifyService;
 import ac.kr.kw.judge.challenge.service.command.ChallengeInfoModifyCommand;
 import ac.kr.kw.judge.challenge.service.command.QuestionRegisterCommand;
+import ac.kr.kw.judge.challenge.service.port.out.EventSender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +21,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ChallengeModifyServiceImpl implements ChallengeModifyService {
     private final ChallengeRepository challengeRepository;
+    private EventSender eventSender;
 
     @Override
     public void changeQuestions(Long challengeId, List<QuestionRegisterCommand> questionRegisterCommands) {
@@ -38,5 +41,12 @@ public class ChallengeModifyServiceImpl implements ChallengeModifyService {
     public void changeChallengeInfo(Long challengeId, ChallengeInfoModifyCommand challengeInfoModifyCommand) {
         Challenge challenge = ChallengeFindHelper.findChallengeById(challengeId, challengeRepository);
         challenge.changeChallengeInfo(challengeInfoModifyCommand.getName(), challengeInfoModifyCommand.getChallengeDateTime());
+
+        ProblemOpenTimeChanged problemOpenTimeChanged = new ProblemOpenTimeChanged(
+                challenge.getChallengeQuestions()
+                        .stream().map(question -> question.getProblemId())
+                        .collect(Collectors.toList()),
+                challenge.getChallengeDateTime().getStartTime());
+        eventSender.publish("problem_open_time_changed", problemOpenTimeChanged);
     }
 }
