265823cdd81c0c0cc66b140bf9c70541df8fe303
bugdebugging <annyousung@gmail.com>
2022-03-28 12:14:18 +0900

    fix:participation register fetch join으로 data가져오도록

--- src/main/java/ac/kr/kw/judge/challenge/service/ParticipationRegisterServiceImpl.java
+++ src/main/java/ac/kr/kw/judge/challenge/service/ParticipationRegisterServiceImpl.java
@@ -4,12 +4,14 @@ import ac.kr.kw.judge.challenge.domain.Challenge;
 import ac.kr.kw.judge.challenge.repository.ChallengeRepository;
 import ac.kr.kw.judge.challenge.service.helper.ChallengeFindHelper;
 import ac.kr.kw.judge.challenge.service.port.in.ParticipationRegisterService;
-import ac.kr.kw.judge.challenge.service.command.ParticipationRegisterCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 
 @Service
 @Transactional
@@ -18,15 +20,15 @@ public class ParticipationRegisterServiceImpl implements ParticipationRegisterSe
     private final ChallengeRepository challengeRepository;
 
     @Override
-    public void participateInChallenge(Long challengeId, ParticipationRegisterCommand participationRegisterCommand) {
-        Challenge challenge = ChallengeFindHelper.findById(challengeId, challengeRepository);
-        challenge.participateInChallenge(participationRegisterCommand.getUserId()
-                , participationRegisterCommand.getName(), LocalDateTime.now());
+    public void participateInChallenge(Long challengeId, String username) {
+        checkArgument(isNotEmpty(username), "사용자 이름은 필수입니다.");
+        Challenge challenge = ChallengeFindHelper.findByIdWithParticipation(challengeId, challengeRepository);
+        challenge.participateInChallenge(username, LocalDateTime.now());
     }
 
     @Override
     public void cancelParticipation(Long challengeId, String username) {
-        Challenge challenge = ChallengeFindHelper.findById(challengeId, challengeRepository);
+        Challenge challenge = ChallengeFindHelper.findByIdWithParticipation(challengeId, challengeRepository);
         challenge.cancelParticipate(username);
     }
 }
