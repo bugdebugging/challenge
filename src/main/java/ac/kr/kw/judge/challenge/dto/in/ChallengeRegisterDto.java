2520302c5c7d72eb2e5d55642c493a2fb738c011
bugdebugging <annyousung@gmail.com>
2022-03-29 11:44:05 +0900

    fix:사용하지않는 변수제거

--- src/main/java/ac/kr/kw/judge/challenge/dto/in/ChallengeRegisterDto.java
+++ src/main/java/ac/kr/kw/judge/challenge/dto/in/ChallengeRegisterDto.java
@@ -1,21 +1,15 @@
 package ac.kr.kw.judge.challenge.dto.in;
 
-import ac.kr.kw.judge.challenge.domain.Author;
 import ac.kr.kw.judge.challenge.domain.ChallengeDateTime;
 
 import java.util.List;
 
 public class ChallengeRegisterDto {
-    private List<Author> authors;
     private List<Long> questions;
     private String name;
     private ChallengeDateTime challengeDateTime;
 
     public ChallengeRegisterDto() {
-    }
-
-    public List<Author> getAuthors() {
-        return authors;
     }
 
     public List<Long> getQuestions() {
