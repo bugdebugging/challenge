57fc1e45c3c74a13802035c7abea180d77db8b38
bugdebugging <annyousung@gmail.com>
2022-04-13 21:37:17 +0900

    fix:잘못된 이벤트 data type 수정

--- src/main/java/ac/kr/kw/judge/challenge/service/ChallengeGradeServiceImpl.java
+++ src/main/java/ac/kr/kw/judge/challenge/service/ChallengeGradeServiceImpl.java
@@ -2,6 +2,7 @@ package ac.kr.kw.judge.challenge.service;
 
 import ac.kr.kw.judge.challenge.domain.Author;
 import ac.kr.kw.judge.challenge.domain.Challenge;
+import ac.kr.kw.judge.challenge.domain.event.ChallengeFinished;
 import ac.kr.kw.judge.challenge.domain.event.ParticipantResult;
 import ac.kr.kw.judge.challenge.repository.ChallengeRepository;
 import ac.kr.kw.judge.challenge.service.helper.ChallengeFindHelper;
@@ -28,8 +29,9 @@ public class ChallengeGradeServiceImpl implements ChallengeGradeService {
         if (!challenge.getAuthor().equals(Author.of(username))) {
             throw new UnAuthorizedException("대회 채점 요청은 대회 개최자만이 수행할 수 있습니다.");
         }
-        eventSender.publish(CHALLENGE_FINISH_TOPIC_NAME, challenge.getParticipations().stream()
+        ChallengeFinished challengeFinished = ChallengeFinished.of(challengeId, challenge.getParticipations().stream()
                 .map(participation -> ParticipantResult.of(participation.getName(), participation.getChallengeScore().getScore()))
                 .collect(Collectors.toList()));
+        eventSender.publish(CHALLENGE_FINISH_TOPIC_NAME, challengeFinished);
     }
 }
